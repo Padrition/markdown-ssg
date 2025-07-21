1b9ae322cbced6b538312e029c76d0e0b245709b
Dmitrii Titarenko <padrition@gmail.com>
2025-07-21 22:10:44 +0200

    (lexer)
    lexing of non special characters to text

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,6 +1,4 @@
-use std::rc::Rc;
-
-use crate::lexer::{ErrorHandler, Token, TokenType, error_handler};
+use crate::lexer::{ErrorHandler, Token, TokenType};
 
 pub struct Lexer<'a> {
     source: String,
@@ -23,6 +21,56 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn scan_token(&mut self) {
+        let c = self.advance();
+        match c {
+            '#' => self.add_token(TokenType::Hash),
+            '*' => self.add_token(TokenType::Star),
+            '_' => self.add_token(TokenType::Underscore),
+            '-' => self.add_token(TokenType::Dash),
+            '~' => self.add_token(TokenType::Tilde),
+            ' ' => {}
+            '\t' => {}
+            '\n' => {
+                self.add_token(TokenType::NewLine);
+                self.line += 1;
+            }
+            _ => {
+                if !self.is_special(c) {
+                    self.scan_text();
+                } else {
+                    self.error_handler
+                        .log_error(self.line, self.current, "Unexpected character.");
+                }
+            }
+        }
+    }
+
+    fn scan_text(&mut self) {
+        while !self.is_at_end() {
+            let c = self.peak();
+
+            if self.is_special(c) {
+                break;
+            }
+
+            self.advance();
+        }
+        self.add_token(TokenType::Content);
+    }
+
+    fn is_special(&self, c: char) -> bool {
+        matches!(c, '#' | '*' | '_' | '-' | '~' | '\0')
+    }
+
+    fn peak(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+
+        self.source.chars().nth(self.current).unwrap()
+    }
+
     pub fn scan_tokens(&mut self) -> Vec<Token> {
         while !self.is_at_end() {
             self.start = self.current;
@@ -39,7 +87,7 @@ impl<'a> Lexer<'a> {
     }
 
     fn advance(&mut self) -> char {
-        let c = self.source.chars().nth(self.current).unwrap();
+        let c = self.peak();
         self.current += 1;
         c
     }
@@ -59,20 +107,5 @@ impl<'a> Lexer<'a> {
 
         self.current += 1;
         true
-    }
-
-    fn scan_token(&mut self) {
-        let c = self.advance();
-        match c {
-            '#' => self.add_token(TokenType::Hash),
-            '*' => self.add_token(TokenType::Star),
-            '_' => self.add_token(TokenType::Underscore),
-            '-' => self.add_token(TokenType::Dash),
-            '~' => self.add_token(TokenType::Tilde),
-            _ => {
-                self.error_handler
-                    .log_error(self.line, self.current, "Unexpected character.");
-            }
-        }
     }
 }
