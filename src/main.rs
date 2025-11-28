7877fa927c225d923881d3a02e860415efb4ba74
Dmitrii Titarenko <padrition@gmail.com>
2025-11-28 22:21:54 +0100

    (better logger)
    added env_logger for debug logging

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ mod run;
 mod transformer;
 
 use run::{run_on_file, run_repl};
+use std::io::Write;
 
 use clap::Parser;
+use env_logger::Builder;
+use log::LevelFilter;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -22,9 +25,22 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
+    Builder::new()
+        .filter_level(if cli.verbose {
+            LevelFilter::max()
+        } else {
+            LevelFilter::Warn
+        })
+        .format(|buf, record| {
+            let warn_style = buf.default_level_style(record.level());
+            let level = record.level().to_string();
+
+            writeln!(buf, "{warn_style}{level}:{warn_style:#}{}", record.args())
+        })
+        .init();
 
     match cli.file {
-        Some(file_name) => run_on_file(&file_name, cli.output.as_deref(), cli.verbose),
+        Some(file_name) => run_on_file(&file_name, cli.output.as_deref()),
         None => run_repl(),
     }
 }
--- src/run.rs
+++ src/run.rs
@@ -3,6 +3,8 @@ use std::io;
 use std::io::Write;
 use std::path::Path;
 
+use log::debug;
+
 use crate::lexer::lexer::Lexer;
 use crate::parser::ast_printer::AstPrinter;
 use crate::parser::parser::Parser;
@@ -10,10 +12,10 @@ use crate::transformer::html_ast_transform::HtmlAstTransformer;
 use crate::transformer::html_node::HtmlNode;
 use crate::transformer::html_transform::HtmlTransformer;
 
-pub fn run_on_file(file_name: &str, output_file_name: Option<&str>, verbose: bool) {
+pub fn run_on_file(file_name: &str, output_file_name: Option<&str>) {
     let source = fs::read_to_string(&file_name).unwrap();
 
-    let html = run(source, verbose);
+    let html = run(source);
 
     let path = output_file_name.map(|s| s.to_string()).unwrap_or_else(|| {
         Path::new(file_name)
@@ -32,39 +34,36 @@ pub fn run_repl() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        run(input, true);
+        println!("Html output:\n{}", run(input));
     }
 }
 
-fn run(source: String, verbose: bool) -> String {
+fn run(source: String) -> String {
     let mut lexer = Lexer::new(source);
     let tokens = lexer.scan_tokens();
 
-    if verbose {
-        println!("Lexer output:");
-        tokens.iter().for_each(|x| print!("{x}"));
-        println!();
-    }
+    debug!(
+        "Lexer output:\n{}\n",
+        tokens.iter().map(|t| t.to_string()).collect::<String>()
+    );
 
     let mut parser = Parser::new(tokens);
     let asts = parser.parse();
     let mut printer = AstPrinter;
 
-    if verbose {
-        println!("Parser output:");
-        asts.iter().for_each(|ast| print!("{}", printer.print(ast)));
-        println!();
-    }
+    debug!(
+        "Parser output:\n{}\n",
+        asts.iter().map(|t| printer.print(t)).collect::<String>()
+    );
 
     let mut ast_trans = HtmlAstTransformer;
 
     let hast: Vec<HtmlNode> = asts.iter().map(|ast| ast_trans.transform(ast)).collect();
 
-    if verbose {
-        println!("Transform to html:");
-        hast.iter().for_each(|h| print!("{:?}", h));
-        println!();
-    }
+    debug!(
+        "Transform to html:\n{}\n",
+        hast.iter().map(|t| format!("{:?}", t)).collect::<String>()
+    );
 
     let html_trans = HtmlTransformer;
 
@@ -74,9 +73,5 @@ fn run(source: String, verbose: bool) -> String {
         .collect::<Vec<_>>()
         .join("");
 
-    if verbose {
-        println!("Html output:");
-        println!("{}", out);
-    }
     out
 }
