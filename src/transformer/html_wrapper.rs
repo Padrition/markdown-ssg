9ee74bb76b8697f790ad6321203a0fcb9c4065fe
Dmitrii Titarenko <padrition@gmail.com>
2025-11-29 20:17:58 +0100

    (dynamic title)
    title will either default to a predefined one or if the first body dag is h1 taken from it and it's children

--- src/transformer/html_wrapper.rs
+++ src/transformer/html_wrapper.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 use crate::transformer::html_node::{Display, HtmlNode};
 
 pub struct HtmlWrapper;
@@ -17,7 +19,7 @@ impl HtmlWrapper {
                         tag: String::from("title"),
                         attrs: None,
                         display: Display::Inline,
-                        children: vec![HtmlNode::Text(String::from("My page"))],
+                        children: vec![HtmlNode::Text(HtmlWrapper::create_title(hast.first()))],
                     }],
                 },
                 HtmlNode::Element {
@@ -27,6 +29,33 @@ impl HtmlWrapper {
                     children: hast,
                 },
             ],
+        }
+    }
+
+    fn create_title(node: Option<&HtmlNode>) -> String {
+        let default_title = String::from("My page");
+
+        let Some(HtmlNode::Element { tag, children, .. }) = node else {
+            return default_title;
+        };
+
+        if tag != "h1" {
+            return default_title;
+        }
+
+        let mut title = String::new();
+
+        HtmlWrapper::collect_title(children, &mut title);
+
+        title
+    }
+
+    fn collect_title(nodes: &Vec<HtmlNode>, out: &mut String) {
+        for child in nodes {
+            match child {
+                HtmlNode::Element { children, .. } => HtmlWrapper::collect_title(children, out),
+                HtmlNode::Text(text) => out.push_str(text),
+            }
         }
     }
 }
